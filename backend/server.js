b329688cba8ac99001a82c13dc2aded8eccb2c6e
Deren B <dbozerprogramming@gmail.com>
2022-03-02 19:13:10 -0500

    Initializing express

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,14 @@
 // ENTRY POINT TO THE SERVER
-console.log("Hello World");
+
+// BRINGS IN EXPRESS & DOTENV
+const express = require('express');
+const dotenv = require('dotenv').config();
+
+// PORT
+const port = process.env.PORT || 5002;
+
+// INITIALIZES EXPRESS
+const app = express();
+
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
