cc9470555219144b07cbb21e0210a87a06b0aa3d
Deren B <dbozerprogramming@gmail.com>
2022-03-02 21:03:22 -0500

    Controller Functions

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,8 +1,35 @@
+// @desc    GET GOALS
+// @route   GET /api/goals
+// @access  Private
 const getGoals = (req, res) => {
     res.status(200).json({ message: 'Get Goals' })
 }
 
+// @desc    SET GOAL
+// @route   POST /api/goals
+// @access  Private
+const setGoal = (req, res) => {
+    res.status(200).json({ message: 'Set Goal' })
+}
+
+// @desc    UPDATE GOAL
+// @route   PUT /api/goals/:id
+// @access  Private
+const updateGoal = (req, res) => {
+    res.status(200).json({ message: `Update goal ${req.params.id}` })
+}
+
+// @desc    DELETE GOAL
+// @route   DELETE /api/goals/:id
+// @access  Private
+const deleteGoal = (req, res) => {
+    res.status(200).json({ message: `Delete goal ${req.params.id}` })
+}
+
 // EXPORTS
 module.exports = {
-    getGoals
+    getGoals,
+    setGoal,
+    updateGoal,
+    deleteGoal
 }
--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,25 +1,11 @@
 // ROUTES
 const express = require('express')
 const router = express.Router()
-const {getGoals} = require('../controllers/goalController')
+const {getGoals, setGoal, updateGoal, deleteGoal} = require('../controllers/goalController')
 
-// GET REQUEST
-router.get('/', getGoals)
-
-// CREATE REQUEST
-router.post('/', (req, res) => {
-    res.status(200).json({ message: 'Set Goal' })
-})
-
-// UPDATE GOALS
-router.put('/:id', (req, res) => {
-    res.status(200).json({ message: `Update goal ${req.params.id}` })
-})
-
-// DELETE REQUEST
-router.delete('/:id', (req, res) => {
-    res.status(200).json({ message: `Delete goal ${req.params.id}` })
-})
+// ROUTE SHORT HAND
+router.route('/').get(getGoals).post(setGoal)
+router.route('/:id').delete(deleteGoal).put(updateGoal)  
 
 
 module.exports = router
